116a8225bae5ecc13ec4a7787d8fa8134b2216c9
michaelecolley <michael.e.colley@googlemail.com>
2021-08-04 00:30:40 +0100

    Remove style elements from index.js and html

--- client/index.js
+++ client/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import { render } from 'react-dom';
-// we will import APP
-import App from './App.jsx'
+import React from "react";
+import { render } from "react-dom";
+import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-import './stylesheets/styles.css';
 
-render (
-    <Router>
-        <App/>
-    </Router>,
-    document.getElementById('root'),
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
-
-
