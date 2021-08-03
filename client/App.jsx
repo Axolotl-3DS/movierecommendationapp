7e57291744ca8a38252d3c938b130b16cef5ef74
michaelecolley <michael.e.colley@googlemail.com>
2021-08-03 22:31:40 +0100

    Reformat spaces and remove to do

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,18 @@
 // Top level React component (wrapper class)
 import React from "react";
 import Main from "./containers/Main.jsx";
-import Home from './containers/Home.jsx';
-import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./containers/Home.jsx";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+// TODO: Understand correct router syntax (should it be wrapping the app component and the switch component?)
 
 const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path='/' component={Main}/>
-                <Route exact path='/home' component={Home}/>
-            </Switch>
-        </Router>
-    ); 
-}
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Main} />
+        <Route exact path='/home' component={Home} />
+      </Switch>
+    </Router>
+  );
+};
 export default App;
