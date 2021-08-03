00b01aaec24fa5b99e8fae5c22cdbb735b803c4d
Adam Goodman <adamrgoodman1@gmail.com>
2021-08-03 17:00:44 -0400

    cleaning up the code - movieController,loginRouter,movierouter

--- server/routes/movieRouter.js
+++ server/routes/movieRouter.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
-router.get('/', movieController.getUserInput, movieController.getRecommendations, movieController.getFavs, (req, res) => res.status(200).json(res.locals));
-router.post('/search', movieController.getSearch, (req, res) => res.status(200).json(res.locals));
+router.get('/',
+    movieController.getUserInput,
+    movieController.getRecommendations,
+    movieController.getFavs,
+    (req, res) => res.status(200).json(res.locals));
+
+
+router.post('/search',
+    movieController.getSearch,
+    (req, res) => res.status(200).json(res.locals));
 
 module.exports = router;
 
