3b533829c0f26011e16429979f2c57d6a83698e3
michaelecolley <michael.e.colley@googlemail.com>
2021-08-05 04:05:39 +0100

    Remove image from login screen background

--- client/containers/Main.jsx
+++ client/containers/Main.jsx
@@ -5,7 +5,6 @@ import Login from "../components/Login.jsx";
 const Main = (props) => {
   return (
     <div id='Main' className='container'>
-      <img default='lots of movie posters'></img>
       <Login />
     </div>
   );
