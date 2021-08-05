1e5cb548f31b9f56d18d08b8c80491c93601cc96
ronke11 <rsoyekunle@gmail.com>
2021-08-05 11:07:05 -0400

    Additional tests

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -4,6 +4,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
 import Login from '../client/components/Login.jsx';
+import Home from '../client/containers/Home.jsx';
 import MovieTile from '../client/components/MovieTile.jsx';
 import MovieContainer from '../client/containers/MovieContainer.jsx';
 
@@ -28,5 +29,34 @@ describe('React unit tests', () => {
       expect(wrapper.find('h1').text()).toEqual('Recommend Me!');
     });
   });
+
+  //Test Home/Search pages
+  describe('Home / Search Page', () => {
+    let wrapper;
+    // const props = {
+
+    // }
+
+    beforeAll(() => {
+      wrapper = shallow(<Home />);
+    });
+
+    // Home page has a movieContainer component
+    it('Renders the Movie Container page', () => {
+      expect(wrapper.find('MovieContainer')).toHaveLength(1);
+    });
+
+    //Search page has a isLoggedIn state
+    it('Renders the Movie Container page', () => {
+      expect(wrapper.find('MovieContainer').state('isLoggedIn')).toBe(true);
+    });
+
+    //Search page has a Searchbox
+    it('Renders the Movie Container page', () => {
+      expect(wrapper.find('MovieContainer').state('isLoggedIn')).toBe(true);
+    });
+  });
+
+
 
 
