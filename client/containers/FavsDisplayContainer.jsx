919b38b22f034a17979815bba756f2835b7f0ad4
ronke11 <rsoyekunle@gmail.com>
2021-08-05 13:33:31 -0400

    Creates favs container

--- client/containers/FavsDisplayContainer.jsx
+++ client/containers/FavsDisplayContainer.jsx
@@ -1,60 +1,44 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import MovieTile from "../components/MovieTile";
+import { useSelector } from 'react-redux'
 import axios from "axios";
 
 function FavsDisplayContainer(props) {
-  const [items, setItems] = useState([]);
-  const [search, setSearch] = useState("");
 
-  // Persist the componentMounted and set to false
-  const componentMounted = useRef(false);
-  let movieCards = [];
-  // right now breaks if user clicks before searching
-  // useEffect(() => {});
-  async function onClickSearch() {
-    if (event.key === "Enter") {
-      let search_format = search.replace(" ", "+");
-      axios
-        .post("/api/search", { title: search_format })
-        .then((res) => {
-          console.log(res);
-          let wrapper = { searches: res.data.searchResults };
-          setItems(res.data.searchResults);
-        })
-        .catch((err) => console.log("App.componentDidMount: ERROR: ", err));
-    }
-  }
+  const [favsList, setFavsList] = useState(['']);
+  // grab the username from the store
+  // then post request to server to grab favs from database
+  useEffect( () => {
+    const username = useSelector(state => state.user.currentUser);
+    axios
+    // .post("/api/favs", { username: username })
+    // .then((res) => {
+    //   console.log(res);
+    //   setFavsList(res.favResults);
+    // })
+    // .catch((err) => console.log("ERROR: ", err));
+  });
+
+  // then I want to display all the favorites;
 
   const renderResults = () => {
     let runOnce = false;
-    if (items.length > 0 && runOnce === false) {
+    if (favsList.length > 0 && runOnce === false) {
       // Renders movieTile with contents of the movies prop
       runOnce = true;
-      return items.map((el, i) => {
+      return favsList.map((el, i) => {
         return <MovieTile className='movieTile' key={i} props={el} />;
       });
     }
   };
 
-  const onChange = (e) => setSearch(e.target.value);
 
   return (
     <div>
-      <div className='searchBar'>
-        <span>
-          <h3>MovieSnek</h3>
-        </span>
-        <input
-          type='text'
-          id='search'
-          placeholder={"search"}
-          onChange={onChange}
-          className='textbox'
-          onKeyDown={onClickSearch}></input>
-      </div>
-      <div className='movieDisplayContainer'>{renderResults()}</div>
+      <h2>Your Favorite Movies!</h2>
+      {/* <div className='favsDisplayContainer'>{renderResults()}</div> */}
     </div>
   );
 }
 
-export default MovieDisplayContainer;
+export default FavsDisplayContainer;
