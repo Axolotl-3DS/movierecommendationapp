07f2973a789a04424b6cfff955e99b2874a466ff
michaelecolley <michael.e.colley@googlemail.com>
2021-08-05 02:41:30 +0100

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
